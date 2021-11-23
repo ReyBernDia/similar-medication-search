a404dda522efc5ca4f28ad5599a94a3d503073f1
reyberndia <reynanikolayev@gmail.com>
2021-11-23 14:18:59 -0800

    Try path.join

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.get("/search/:term", async (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
